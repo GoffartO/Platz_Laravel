ffcd20942b79f0aceba1cf46cbf8b77f1e6e8cfb
Olivia <oliviagoffart@gmail.com>
2019-06-13 11:11:16 +0200

    Ressources similaires
    Ajout des ressources similaires

--- platz/app/Http/Controllers/RessourcesController.php
+++ platz/app/Http/Controllers/RessourcesController.php
@@ -15,9 +15,20 @@
             return View::make('ressources.index', ['ressources' => $ressources ]);
          }
 
-         public function show($id = 1){
+         public function show($id){
             $ressource = RessourcesMdl::with('users')->find($id);
-            return View::make('ressources.show', ['ressource' => $ressource ]);
+
+            // Trouver les ressources avec la même catégorie et en excluant la catégorie actuelle
+            $ressourcesRelated = RessourcesMdl::with('categories')
+                                              ->where([
+                                                      ['categorie',$ressource->categorie],
+                                                      ['id','<>',$ressource->id]
+                                                    ])
+                                              ->take(4)
+                                              ->get();
+            return View::make('ressources.show', ['ressource' => $ressource,
+                                                  'ressourcesRelated' => $ressourcesRelated  ]);
+
          }
 
 
